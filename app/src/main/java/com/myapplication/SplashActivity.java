36d1ff09a61d13176068a5b21b8dac07cafc98db
Vivek Jha <vivek.g621@gmail.com>
2019-02-24 21:25:34 +0530

    add splash screen

--- app/src/main/java/com/myapplication/SplashActivity.java
+++ app/src/main/java/com/myapplication/SplashActivity.java
@@ -1,13 +1,41 @@
 package com.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.myapplication.home.HomeActivity;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private Runnable runnable;
+    private Handler handler;
+    private static final int DELAY = 3000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               runnable = this;
+               startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+               finish();
+            }
+        },DELAY);
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(runnable);
+    }
+
+    @Override
+    public void onBackPressed() {}
+
 }
